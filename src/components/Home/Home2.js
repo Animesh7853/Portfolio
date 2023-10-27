0ee190e12e30347deede2b8c8820e29a28634cc7
Animesh Jha <114354311+Animesh7853@users.noreply.github.com>
2023-10-28 02:36:46 +0530

    home

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,7 +24,7 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and . </b>
+                <b className="purple"> C++, Javascript and Python. </b>
               </i>
               <br />
               <br />
